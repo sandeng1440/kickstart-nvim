cbb2512a20fb2d3ff2f19abe9b13f1b275134cfa
sandeng1440 <sandeng1440@gmail.com>
2024-04-24 22:05:45 +0300

    Auto commit

--- init.lua
+++ init.lua
@@ -47,7 +47,7 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>de', vim.diagnostic.open_float, { desc = 'Show [D]iagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
@@ -116,9 +116,7 @@ require('lazy').setup {
   require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
   { import = 'custom.plugins' },
-  --vim.cmd [[colorscheme miasma]],
 }
-vim.cmd [[colorscheme moonfly]]
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,69 +1,13 @@
 -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
 -- You can configure highlights by doing something like
 -- vim.cmd.hi 'Comment gui=none'
-
 return {
-  -- { 'catppuccin/nvim', name = 'catppuccin' },
-  {
-    'xero/miasma.nvim',
-    name = 'miasma',
-    lazy = false,
-    priority = 1000,
-    -- config = function()
-    --   vim.cmd.colorscheme 'miasma'
-    -- end,
-  },
-  {
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    opts = {
-      term_colors = true,
-      transparent_background = false,
-      styles = {
-        comments = {},
-        conditionals = {},
-        loops = {},
-        functions = {},
-        keywords = {},
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-      },
-      color_overrides = {
-        mocha = {
-          base = '#000000',
-          mantle = '#000000',
-          crust = '#000000',
-        },
-      },
-      integrations = {
-        telescope = {
-          enabled = true,
-          style = 'nvchad',
-        },
-        dropbar = {
-          enabled = true,
-          color_mode = true,
-        },
-      },
-    },
-  },
-  { 'bluz71/vim-moonfly-colors', name = 'moonfly', lazy = false, priority = 1000 },
-  { 'scottmckendry/cyberdream.nvim', name = 'cyberdream', lazy = false, priority = 1000 },
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = {},
-    config = function()
-      -- local cyberdream = require 'lualine.themes.cyberdream'
-      require('lualine').setup {
-        options = {
-          theme = 'moonfly',
-        },
-      }
-    end,
-  },
+  'bluz71/vim-moonfly-colors',
+  name = 'moonfly',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.cmd [[colorscheme moonfly]]
+    -- vim.g.moonflyCursorColor = true
+  end,
 }
